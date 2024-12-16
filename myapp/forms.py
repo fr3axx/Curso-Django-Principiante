23998c27593c77fb9a024951fb857c96e4baef1c
fr3axx <rodriguezdavidr847@gmail.com>
2024-12-15 22:57:43 -0400

    mejoras finales

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Titulo de Tarea", max_length=200)
-    description = forms.CharField(label="Descripcion de la Tarea", widget=forms.Textarea)
+    title = forms.CharField(label="Titulo de Tarea", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
+    description = forms.CharField(label="Descripcion de la Tarea", widget=forms.Textarea(attrs={'class': 'input'}))
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label="Nombre del Proyecto", max_length=200)
+    name = forms.CharField(label="Nombre del Proyecto", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
